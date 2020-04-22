5d96283d271027a2fb95a12a40de92356f61b5ae
Choegyel Norbu <choegyel.norbu@dcpl.com>
2020-04-22 13:47:35 +0600

    initial commit

---
+++ user_interface/src/components/landing.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import './css/landingpage.css';
+
+class Landing extends Component{
+  render() {
+    return(
+      <div>
+        <div className=''></div>
+      </div>
+    )
+  }
+}
+
+export default Landing
